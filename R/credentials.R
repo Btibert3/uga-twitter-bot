9948d6d137e9e3490dc0817cc3a9a89e88b013f7
Brock Tibert <Btibert3@gmail.com>
2014-05-23 12:34:34 -0400

    Revert "migrate to R solution"
    This reverts commit b10dec605a2b310f019d0e409dbcbe370d8cf8f5.

--- R/credentials.R
+++
@@ -1,8 +0,0 @@
-## twitter api info for jonsmith_3
-consumer_key        = ""
-consumer_secret     = ""
-access_token        = ""
-access_token_secret = ""
-
-## set the KEY for our Google Spreadsheet that acts like an easy dataset
-KEY = ""

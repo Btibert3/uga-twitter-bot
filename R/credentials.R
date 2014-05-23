b10dec605a2b310f019d0e409dbcbe370d8cf8f5
Brock Tibert <Btibert3@gmail.com>
2014-05-23 12:33:33 -0400

    migrate to R solution
    Abandoned python tweedy for twitteR and streamR

---
+++ R/credentials.R
@@ -0,0 +1,8 @@
+## twitter api info for jonsmith_3
+consumer_key        = ""
+consumer_secret     = ""
+access_token        = ""
+access_token_secret = ""
+
+## set the KEY for our Google Spreadsheet that acts like an easy dataset
+KEY = ""
